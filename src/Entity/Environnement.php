73f2f87bc43b18520fa10fb2a591d3b20b6adc00
sodes <sodes@SoDesuu>
2025-02-19 16:27:49 +0100

    Phase 15 : ORM (Environnements : ajout/suppr)

--- src/Entity/Environnement.php
+++ src/Entity/Environnement.php
@@ -1,10 +1,7 @@
 <?php
-
 namespace App\Entity;
-
 use App\Repository\EnvironnementRepository;
 use Doctrine\ORM\Mapping as ORM;
-
 #[ORM\Entity(repositoryClass: EnvironnementRepository::class)]
 class Environnement
 {
@@ -12,24 +9,19 @@ class Environnement
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-
     #[ORM\Column(length: 100)]
     private ?string $nom = null;
-
     public function getId(): ?int
     {
         return $this->id;
     }
-
     public function getNom(): ?string
     {
         return $this->nom;
     }
-
     public function setNom(string $nom): static
     {
         $this->nom = $nom;
-
         return $this;
     }
 }
--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace App\Entity;
+
 use App\Repository\VisiteRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+
 #[ORM\Entity(repositoryClass: VisiteRepository::class)]
 class Visite
 {
@@ -12,18 +15,25 @@ class Visite
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
+
     #[ORM\Column(length: 50)]
     private ?string $ville = null;
+
     #[ORM\Column(length: 50)]
     private ?string $pays = null;
+
     #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
     private ?\DateTimeInterface $datecreation = null;
+
     #[ORM\Column(nullable: true)]
     private ?int $note = null;
+
     #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $avis = null;
+
     #[ORM\Column(nullable: true)]
     private ?int $tempmin = null;
+
     #[ORM\Column(nullable: true)]
     private ?int $tempmax = null;
 
@@ -32,7 +42,6 @@ class Visite
      */
     #[ORM\ManyToMany(targetEntity: Environnement::class)]
     private Collection $environnements;
-
     public function __construct()
     {
         $this->environnements = new ArrayCollection();
@@ -41,35 +50,44 @@ class Visite
     {
         return $this->id;
     }
+
     public function getVille(): ?string
     {
         return $this->ville;
     }
+
     public function setVille(string $ville): static
     {
         $this->ville = $ville;
+
         return $this;
     }
+
     public function getPays(): ?string
     {
         return $this->pays;
     }
+
     public function setPays(string $pays): static
     {
         $this->pays = $pays;
+
         return $this;
     }
+
     public function getDatecreation(): ?\DateTimeInterface
     {
         return $this->datecreation;
     }
+
     public function setDatecreation(?\DateTimeInterface $datecreation): static
     {
         $this->datecreation = $datecreation;
+
         return $this;
     }
-    
-        public function getDatecreationString(): string
+
+    public function getDatecreationString(): string
     {
         if($this->datecreation == null){
             return "";
@@ -77,44 +95,54 @@ class Visite
             return $this->datecreation->format('d/m/Y');
         }
     }
-    
+
     public function getNote(): ?int
     {
         return $this->note;
     }
+
     public function setNote(?int $note): static
     {
         $this->note = $note;
+
         return $this;
     }
+
     public function getAvis(): ?string
     {
         return $this->avis;
     }
+
     public function setAvis(?string $avis): static
     {
         $this->avis = $avis;
+
         return $this;
     }
+
     public function getTempmin(): ?int
     {
         return $this->tempmin;
     }
+
     public function setTempmin(?int $tempmin): static
     {
         $this->tempmin = $tempmin;
+
         return $this;
     }
+
     public function getTempmax(): ?int
     {
         return $this->tempmax;
     }
+
     public function setTempmax(?int $tempmax): static
     {
         $this->tempmax = $tempmax;
+
         return $this;
     }
-
     /**
      * @return Collection<int, Environnement>
      */
@@ -122,20 +150,16 @@ class Visite
     {
         return $this->environnements;
     }
-
     public function addEnvironnement(Environnement $environnement): static
     {
         if (!$this->environnements->contains($environnement)) {
             $this->environnements->add($environnement);
         }
-
         return $this;
     }
-
     public function removeEnvironnement(Environnement $environnement): static
     {
         $this->environnements->removeElement($environnement);
-
         return $this;
     }
 }
